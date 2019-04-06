c5f60cc185875d27d8795bf841556498b0e94090
Miguel <return.infox@gmail.com>
2019-04-06 02:45:34 -0300

    Añadiendo codigo de error en post

--- server/server.js
+++ server/server.js
@@ -14,6 +14,14 @@ app.get('/usuario', (req, res)=>{
 
 app.post('/usuario', (req, res)=>{
   let body = req.body;
+  if(body.nombre == undefined){
+    res.status(400).json({
+      ok: false,
+      mensaje: 'El nombre es necesario'
+    });
+  }else{
+
+  }
   res.json({
     body
   })
